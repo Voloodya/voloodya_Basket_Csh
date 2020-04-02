67e58d1f5b301deca57df7c4ff40cc05a37441f1
vladimir <vladimir.s.rus@gmail.com>
2020-04-02 16:08:51 +0500

    Add all files

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Shop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridViewStore = new System.Windows.Forms.DataGridView();
+            this.storeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridViewOrder = new System.Windows.Forms.DataGridView();
+            this.AddOrder = new System.Windows.Forms.Button();
+            this.textBoxSumm = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonClearOrder = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonDell = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStore)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.storeBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrder)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewStore
+            // 
+            this.dataGridViewStore.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStore.Location = new System.Drawing.Point(24, 84);
+            this.dataGridViewStore.Name = "dataGridViewStore";
+            this.dataGridViewStore.Size = new System.Drawing.Size(245, 290);
+            this.dataGridViewStore.TabIndex = 0;
+            this.dataGridViewStore.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridViewStore_MouseDoubleClick);
+            // 
+            // dataGridViewOrder
+            // 
+            this.dataGridViewOrder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOrder.Location = new System.Drawing.Point(439, 84);
+            this.dataGridViewOrder.Name = "dataGridViewOrder";
+            this.dataGridViewOrder.Size = new System.Drawing.Size(441, 290);
+            this.dataGridViewOrder.TabIndex = 1;
+            // 
+            // AddOrder
+            // 
+            this.AddOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AddOrder.Location = new System.Drawing.Point(289, 220);
+            this.AddOrder.Name = "AddOrder";
+            this.AddOrder.Size = new System.Drawing.Size(131, 23);
+            this.AddOrder.TabIndex = 2;
+            this.AddOrder.Text = "Добавить всё";
+            this.AddOrder.UseVisualStyleBackColor = true;
+            this.AddOrder.Click += new System.EventHandler(this.AddAllOrder_Click);
+            // 
+            // textBoxSumm
+            // 
+            this.textBoxSumm.Location = new System.Drawing.Point(581, 401);
+            this.textBoxSumm.Name = "textBoxSumm";
+            this.textBoxSumm.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSumm.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Matura MT Script Capitals", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(435, 401);
+            this.label1.MinimumSize = new System.Drawing.Size(120, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Сумма заказа:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(21, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Поиск:";
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSearch.Location = new System.Drawing.Point(79, 22);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(173, 21);
+            this.textBoxSearch.TabIndex = 6;
+            this.textBoxSearch.TextChanged += new System.EventHandler(this.textBoxSearch_TextChanged);
+            // 
+            // buttonClearOrder
+            // 
+            this.buttonClearOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonClearOrder.Location = new System.Drawing.Point(707, 398);
+            this.buttonClearOrder.Name = "buttonClearOrder";
+            this.buttonClearOrder.Size = new System.Drawing.Size(159, 23);
+            this.buttonClearOrder.TabIndex = 7;
+            this.buttonClearOrder.Text = "Очистить корзину";
+            this.buttonClearOrder.UseVisualStyleBackColor = true;
+            this.buttonClearOrder.Click += new System.EventHandler(this.buttonClearOrder_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(21, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Товары:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(436, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Корзина:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 398);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Выбор - 2-ой щелчек";
+            // 
+            // buttonDell
+            // 
+            this.buttonDell.Location = new System.Drawing.Point(289, 271);
+            this.buttonDell.Name = "buttonDell";
+            this.buttonDell.Size = new System.Drawing.Size(131, 44);
+            this.buttonDell.TabIndex = 11;
+            this.buttonDell.Text = "Удалить из корзины выбранное";
+            this.buttonDell.UseVisualStyleBackColor = true;
+            this.buttonDell.Click += new System.EventHandler(this.buttonDell_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 450);
+            this.Controls.Add(this.buttonDell);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonClearOrder);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSumm);
+            this.Controls.Add(this.AddOrder);
+            this.Controls.Add(this.dataGridViewOrder);
+            this.Controls.Add(this.dataGridViewStore);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStore)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.storeBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrder)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewStore;
+        private System.Windows.Forms.BindingSource storeBindingSource;
+        private System.Windows.Forms.DataGridView dataGridViewOrder;
+        private System.Windows.Forms.Button AddOrder;
+        private System.Windows.Forms.TextBox textBoxSumm;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonClearOrder;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonDell;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shop
+{   
+    public partial class Form1 : Form
+    {
+
+        List<Product> listProducts = new List<Product>();
+        Order order = new Order(new Dictionary<Product, (int, float)>());
+        
+        public Form1()
+        {
+            InitializeComponent();            
+            
+            StreamReader fileStream = new StreamReader("inputData.txt", Encoding.GetEncoding("Windows-1251"));
+            string s = "";
+
+            while (!fileStream.EndOfStream)
+            {
+               s = fileStream.ReadLine();               
+               listProducts.Add(new Product(s.Substring(0, s.IndexOf(' ')), float.Parse(s.Substring(s.IndexOf(' ') + 1))));
+            }
+            dataGridViewStore.DataSource = listProducts;
+        }
+
+        public class Product
+        {
+            private string name;
+            private float price;
+
+            public Product(string name, float price)
+            {
+                this.name = name;
+                this.price = price;
+            }
+
+            public string Name
+            {
+                get
+                {
+                    return this.name;
+                }
+                set
+                {
+                    this.name = value;
+                }
+            }
+
+            public float Price
+            {
+                get
+                {
+                    return this.price;
+                }
+                set
+                {
+                    this.price = value;
+                }
+            }
+        }
+
+        public class Order
+        {
+            private Dictionary<Product, (int,float)> order_products;
+
+            public Order(Dictionary<Product, (int,float)> order_products)
+            {
+                this.order_products = order_products;
+            }
+
+            public Dictionary<Product, (int, float)> Order_products
+            {
+                get
+                {
+                    return this.order_products;
+                }
+                set
+                {
+                    this.order_products = value;
+                }
+            }
+
+            public void addProduct(Product product)
+            {
+                if (this.order_products.ContainsKey(product))
+                {
+                    int count = this.order_products[product].Item1;
+                    this.order_products[product] = (++count, product.Price * count);
+                }
+                else this.order_products.Add(product, (1, product.Price));
+            }
+
+            public void addProducts(List<Product> products)
+            {
+                foreach (Product product in products)
+                {
+                     this.order_products.Add(product, (1, product.Price));
+                }
+            }
+
+            public void dellProduct(String name=null, float price = 0)
+            {
+                if (name == null) this.order_products.Clear();
+                else this.order_products.Remove(this.order_products.First(x => x.Key.Name.Equals(name) & x.Key.Price == price).Key);
+            }
+
+            public float summOrder()
+            {
+                float summAll = 0;
+                foreach (KeyValuePair<Product, (int, float)> keyValuePair in this.order_products)
+                {
+                    summAll += keyValuePair.Value.Item2;
+                }
+
+                return summAll;
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridViewStore_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int idStr = dataGridViewStore.CurrentRow.Index;
+            Product prdct = listProducts.Find(x => (x.Name.Equals((string)dataGridViewStore.Rows[idStr].Cells[0].Value) &
+                                              x.Price == (float)dataGridViewStore.Rows[idStr].Cells[1].Value));
+
+            order.addProduct(prdct);
+            
+            dataGridViewOrder.DataSource = order.Order_products.Select(x => new { x.Key.Name, x.Key.Price, x.Value.Item1,x.Value.Item2}).ToList();
+            dataGridViewOrder.Columns[2].HeaderText = "Count";
+            dataGridViewOrder.Columns[3].HeaderText = "Summ";
+
+
+            textBoxSumm.Text = order.summOrder().ToString();
+        }
+
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+            String nameProduct = textBoxSearch.Text;
+            dataGridViewStore.DataSource = listProducts.FindAll(x => x.Name.Contains(nameProduct)).ToList();
+        }
+
+       
+        private void AddAllOrder_Click(object sender, EventArgs e)
+        {
+            order.addProducts(listProducts);
+            dataGridViewOrder.DataSource = order.Order_products.Select(x => new { x.Key.Name, x.Key.Price, x.Value.Item1, x.Value.Item2}).ToList();
+            dataGridViewOrder.Columns[2].HeaderText = "Count";
+            dataGridViewOrder.Columns[3].HeaderText = "Summ";
+
+            textBoxSumm.Text = order.summOrder().ToString();
+        }
+
+        private void buttonClearOrder_Click(object sender, EventArgs e)
+        {
+            order.dellProduct();
+            dataGridViewOrder.DataSource = null;
+            textBoxSumm.Text = "0";
+        }
+
+
+        private void buttonDell_Click(object sender, EventArgs e)
+        {            
+            int idStr = dataGridViewOrder.CurrentRow.Index;
+            order.dellProduct((string)dataGridViewOrder.Rows[idStr].Cells[0].Value, (float)dataGridViewOrder.Rows[idStr].Cells[1].Value);            
+            dataGridViewOrder.DataSource = order.Order_products.Select(x => new { x.Key.Name, x.Key.Price, x.Value.Item1, x.Value.Item2 }).ToList();
+            dataGridViewOrder.Columns[2].HeaderText = "Count";
+            dataGridViewOrder.Columns[3].HeaderText = "Summ";
+            textBoxSumm.Text = order.summOrder().ToString();
+
+        }
+    }
+}
